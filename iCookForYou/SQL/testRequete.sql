003d71da1244c765bbbced7c4a39a6f28b9e0551
clebegue <lebegue.clement@gmail.com>
2018-01-24 15:20:04 +0100

    Auto stash before merge of "master" and "origin/master"

--- iCookForYou/SQL/testRequete.sql
+++ iCookForYou/SQL/testRequete.sql
@@ -1,25 +1,40 @@
-SELECT U.nom, U.prenom, A.idTAllergie, A.nom 
-FROM TUser AS U, TAllergie AS A, TUserAllergie AS UA
-WHERE U.idTUser = '1' 
-AND U.idTUser = UA.idTUserUA 
-AND UA.idTAllergieUA = A.idTAllergie
 
+#Liste des aliments avec leurs quantités dans le stock de l'user 1
+SELECT U.name, U.firstname, F.idFood, F.name, F.quantity, F.unit, S.quantityS
+FROM TUser AS U, TFood AS F, TStock AS S
+WHERE U.idUser = '1'
+AND U.idUser = S.idUserS
+AND S.idFood = F.idFood;
 
-SELECT U.nom, U.prenom, ALI.nom, ALI.quantite, ALI.unite, S.stock
-FROM TUser AS U, TAliment AS ALI, TStock AS S
-WHERE U.idTUser = '0'
-AND U.idTUser = S.idUserS
-AND S.idAliment = ALI.idAliment
+#Quantité totale d'un aliment X pour l'utilisateur 1
+SELECT U.name, U.firstname, F.name, (F.quantity*S.quantityS) AS QuantityTotal, F.unit
+FROM TUser AS U, TFood AS F, TStock AS S
+WHERE U.idUser = '1'
+AND S.idFood = F.idFood
+AND F.name = "lait";
 
+#Quantité totale des aliments dans le stock de l'utilisateur 1
+SELECT U.name, U.firstname, F.name, (F.quantity*S.quantityS) AS QuantityTotal, F.unit
+FROM TUser AS U, TFood AS F, TStock AS S
+WHERE U.idUser = '1'
+AND U.idUser = S.idUserS
+AND S.idFood = F.idFood;
 
-INSERT INTO `TAllergie` (`idTAllergie`, `nom`) VALUES
-(6, 'poissons');
+#Afficher l'historique des recettes de l'user 1
+SELECT U.name, U.firstname, H.idUserH, H.idRecipeH
+FROM TUser AS U, THistory AS H
+WHERE U.idUser = '1'
+AND U.idUser = H.idUserH;
 
-INSERT INTO `TUserAllergie` (`idTUserUA`, `idTAllergieUA`) VALUES
-(0, 6);
+#Compter le nombre de recettes réalisé par l'user 1
+SELECT U.name, U.firstname, COUNT(idRecipeH) AS nbRecettes 
+FROM TUser AS U, THistory AS H
+WHERE U.idUser = '1'
+AND U.idUser = H.idUserH;
 
-INSERT INTO `TUser` (`idTUser`, `nom`, `prenom`, `mail`, `password`) VALUES
-(1, 'pecqueux', 'nathan', 'nathan@gmail.com', 'nathan');
+#Afficher les recettes favorites de l'user 1
+SELECT U.name, U.firstname, F.idUserF, F.idRecipeF
+FROM TUser AS U, TFavorites AS F
+WHERE U.idUser = '1'
+AND U.idUser = F.idUserF;
 
-INSERT INTO `TUserAllergie` (`idTUserUA`, `idTAllergieUA`) VALUES
-(1, 6);
