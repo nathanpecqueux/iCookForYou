646f0dd56ef31282b38a3002407f58841e73fe52
Leblanc Alexandre <alexandre.leblanc.al@gmail.com>
2018-01-24 11:15:31 +0100

    Page Connexion
    Création page connexion

--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/SplashActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/SplashActivity.java
@@ -2,7 +2,6 @@ package fr.univ_littoral.nathan.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -15,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, ConnectionActivity.class);
         startActivity(intent);
         finish();
     }
