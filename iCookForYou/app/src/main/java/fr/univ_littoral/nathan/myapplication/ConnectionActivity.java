ea5ca32ae59a40d66fb1c4453826e7219ba766f3
Leblanc Alexandre <alexandre.leblanc.al@gmail.com>
2018-01-24 11:39:49 +0100

    Commit avant merge

--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/ConnectionActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/ConnectionActivity.java
@@ -1,15 +1,13 @@
 package fr.univ_littoral.nathan.myapplication;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ConnectionActivity extends Activity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connection);
     }
 }
--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/RegisterActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/RegisterActivity.java
@@ -1,9 +1,10 @@
 package fr.univ_littoral.nathan.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
