908aa4f00a4fe6e1196f89f3fadeee590cda0209
Unknown <unknown@example.com>
2018-03-06 10:07:36 +0100

    Nouvelle branche onecub animation
    Mise en place de l'animation de chargement

--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/OneCubActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/OneCubActivity.java
@@ -1,15 +1,16 @@
 package fr.univ_littoral.nathan.myapplication;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
-public class OneCubActivity extends AppCompatActivity implements View.OnClickListener{
+public class OneCubActivity extends Activity implements View.OnClickListener{
     private Button buttonOui;
     private Button buttonNon;
-
+    private ProgressBar spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +19,9 @@ public class OneCubActivity extends AppCompatActivity implements View.OnClickLis
 
         buttonOui = (Button) findViewById(R.id.buttonOui);
         buttonNon = (Button) findViewById(R.id.buttonNon);
+        spinner = (ProgressBar) findViewById(R.id.progressBar);
+
+        spinner.incrementProgressBy(10);
 
         buttonOui.setOnClickListener(this);
         buttonNon.setOnClickListener(this);
@@ -28,7 +32,7 @@ public class OneCubActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.buttonOui:
-                //Page internet OneCub
+                spinner.setVisibility(View.VISIBLE);
                 break;
             case R.id.buttonNon:
                 Intent intentAccueil = new Intent( OneCubActivity.this, HomeActivity.class);
