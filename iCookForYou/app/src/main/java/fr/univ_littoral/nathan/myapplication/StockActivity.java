5946e1f61069a05e7a5e7d0fe1f62b5d37d9898f
Nathan <pecqueux.nathan@hotmail.fr>
2018-03-18 19:31:46 +0100

    Les ingrédients sont réapparus ! :P

--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/StockActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/StockActivity.java
@@ -51,7 +51,7 @@ public class StockActivity extends AppCompatActivity implements View.OnClickList
         modifyStock = (Button) findViewById(R.id.modifIngredient);
         modifyStock.setOnClickListener(this);
 
-        System.out.println("create stock");
+        findFood();
     }
 
     @Override
