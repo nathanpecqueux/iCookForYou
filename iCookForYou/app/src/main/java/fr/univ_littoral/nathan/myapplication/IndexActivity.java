66519c91bf8a2a99168728983fed86649086f0cd
François <bomy.francois@gmail.com>
2018-03-23 10:43:58 +0100

    Menu index

--- iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/IndexActivity.java
+++ iCookForYou/app/src/main/java/fr/univ_littoral/nathan/myapplication/IndexActivity.java
@@ -1,11 +1,17 @@
 package fr.univ_littoral.nathan.myapplication;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IndexActivity extends Activity implements View.OnClickListener {
 
@@ -18,6 +24,53 @@ public class IndexActivity extends Activity implements View.OnClickListener {
         buttonCestParti=(Button) findViewById(R.id.buttonCestParti);
 
         buttonCestParti.setOnClickListener(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_index, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuIndexProfil:
+                Intent intentAccueil = new Intent(IndexActivity.this, ProfileActivity.class);
+                startActivity(intentAccueil);
+                break;
+            case R.id.menuIndexStock:
+                Intent intentStock = new Intent(IndexActivity.this, StockActivity.class);
+                startActivity(intentStock);
+                break;
+            case R.id.menuIndexAPropos:
+                Dialog dialog = new Dialog(this);
+                dialog.setContentView(R.layout.layout_propos);
+                dialog.setCancelable(true);
+                //there are a lot of settings, for dialog, check them all out!
+                dialog.setTitle("A propos de l'application");
+                //set up text
+                TextView text = (TextView) dialog.findViewById(R.id.TextView01);
+                text.setText("Application créée par :");
+                TextView text2 = (TextView) dialog.findViewById(R.id.TextView02);
+                text2.setText("Bomy François\nLebegue Clément\nLeblanc Alexandre\nPecqueux Nathan");
+                TextView text3 = (TextView) dialog.findViewById(R.id.TextView03);
+                text3.setText("Version 1.0,  02/02/2018");
+
+                //set up image view
+                ImageView img = (ImageView) dialog.findViewById(R.id.ImageView01);
+                img.setImageResource(R.drawable.logo_propos);
+
+                dialog.show();
+                break;
+            case R.id.menuIndexQuitter:
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
+        return true;
     }
 
     @Override
